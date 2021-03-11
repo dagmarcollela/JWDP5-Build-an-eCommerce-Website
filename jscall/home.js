126849ac9206b408fd60ee17fb8c79fe508b36d8
Dagmar Collela <dagmamirk@gmail.com>
2021-03-11 13:33:49 +0000

    Create home.js
    If server online unblock the button and hidde the label

---
+++ jscall/home.js
@@ -0,0 +1,25 @@
+const activeButton = document.getElementById("btn-goto-shopping");
+
+activeHomePgButton = (response) => {
+    if(response != null){
+        activeButton.disabled = false;
+    }else{
+        console.log("Is not working");
+        console.log(response);
+    }
+}
+
+init = async () => {
+    try{
+        const requestPromise = getApi();
+        const response = await requestPromise;
+        activeHomePgButton(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+init();
+
+activeButton.addEventListener('click', () => {
+    window.location.assign("pages/shop.html");
+})
