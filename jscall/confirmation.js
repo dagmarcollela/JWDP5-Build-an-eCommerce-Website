bf02f145d89d6ac58fdbead8559040749cadedd4
Dagmar Collela <dagmamirk@gmail.com>
2021-03-26 12:17:30 +0000

    Update confirmation.js

--- jscall/confirmation.js
+++ jscall/confirmation.js
@@ -5,15 +5,15 @@ const customerAddress = document.getElementById("customerAddress");
 const customerAddress2 = document.getElementById("customerAddress2");
 const city = document.getElementById("city");
 
-let index = localStorage.length;
+let index = localStorage.length; // get the last item in the localStorage
 
-let data = localStorage.getItem(index);
+let data = localStorage.getItem(index); // get the last data stored
 data = JSON.parse(data);
 
 
 customerOrder.innerHTML += "Your order number: " + "<b>" + data.code + "</b>";
 customerName.innerHTML += "Customer name: " + "<b>" + data.name + "</b>";
-customerPaid.innerHTML += "Order value: " + "<b>" + "&#36;" + data.total +"</b>";
+customerPaid.innerHTML += "Order value: " + "<b>" + "&#36;" + data.total + "</b>";
 customerAddress.innerHTML += "House number/name: " + "<b>" + data.address + "</b>";
 customerAddress2.innerHTML += "Road name: " + "<b>" + data.address2 + "</b>";
 city.innerHTML += "City: " + "<b>" + data.city + "</b>";
